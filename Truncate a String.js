b4e5e127a899eb430cb3b200c308982bd2343523
Carina Cruz <lecruz@bu.edu>
2017-12-15 04:02:58 +0000

    Truncate a String

---
+++ Truncate a String.js
@@ -0,0 +1,22 @@
+// function truncateString(str, num) {
+//   // Clear out that junk in your trunk
+//   return str;
+// }
+
+// truncateString("A-tisket a-tasket A green and yellow basket", 11);
+
+// _____________________________________________
+
+
+
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  if (str.length > num && num > 3) {
+    return str.slice(0, (num - 3)) + '...';
+  } else if (str.length > num && num <= 3) {
+    return str.slice(0, num) + '...';
+  } else {
+    return str;
+  }
+
+}
